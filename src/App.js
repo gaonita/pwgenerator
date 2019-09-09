95da6b8b54dad4dda5460f4b92decd55a048dae1
gaonita <gaon.yg@gmail.com>
2019-09-09 17:06:40 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {adjectives, adverb, specials, subjectives, verb} from "./dictionary";
+import Header from "./Header";
 
 class App extends React.Component {
     constructor(props) {
@@ -40,6 +41,7 @@ class App extends React.Component {
 
     render() {
 
+
         const pwNum = Math.floor((Math.random() * 10));
         const pwAdj1 = App.randomeEl(adjectives);
         const pwAdj2 = App.randomeEl(adjectives);
@@ -53,45 +55,53 @@ class App extends React.Component {
         if (!this.state.visibility) {
 
             return (
-                <div className="content">
+                <div>
+                    <Header/>
+                    <div className="content">
 
-                    <div className="passwordBox">
-                        <div id="password">Number:</div>
-                        <div id="password">Adjective01:</div>
-                        <div id="password">Adjective02:</div>
-                        <div id="password">Subjective:</div>
-                        <div id="password">Verb:</div>
-                        <div id="password">Adverb:</div>
-                        <div id="password">Special:</div>
+                        <div className="passwordBox">
+                            <div id="password">Number:</div>
+                            <div id="password">Adjective01:</div>
+                            <div id="password">Adjective02:</div>
+                            <div id="password">Subjective:</div>
+                            <div id="password">Verb:</div>
+                            <div id="password">Adverb:</div>
+                            <div id="password">Special:</div>
+                        </div>
+
+                        <div className="entropy">
+                            <p>Entropy:{entropy}</p>
+                        </div>
+
+                        <button id="generate-button" onClick={this.show}>Skapa lösenord</button>
                     </div>
-
-                    <div className="entropy">
-                        <p>Entropy:{entropy}</p>
-                    </div>
-
-                    <button id="generate-button" onClick={this.show}>Skapa lösenord</button>
                 </div>
+
             )
         }
         else {
             return (
-                <div className="content">
-                    <div className="passwordBox">
-                        <div id="password">Number:<br/> {pwNum} </div>
-                        <div id="password">Adjective01:<br/> {pwAdj1} </div>
-                        <div id="password">Adjective02:<br/> {pwAdj2} </div>
-                        <div id="password">Subjective:<br/> {pwSub} </div>
-                        <div id="password">Verb:<br/> {pwVerb} </div>
-                        <div id="password">Adverb:<br/> {pwAdv} </div>
-                        <div id="password">Special:<br/> {pwSpecial}</div>
-                    </div>
+                <div>
+                    <Header/>
+                    <div className="content">
+                        <div className="passwordBox">
+                            <div id="password">Number:<br/> {pwNum} </div>
+                            <div id="password">Adjective01:<br/> {pwAdj1} </div>
+                            <div id="password">Adjective02:<br/> {pwAdj2} </div>
+                            <div id="password">Subjective:<br/> {pwSub} </div>
+                            <div id="password">Verb:<br/> {pwVerb} </div>
+                            <div id="password">Adverb:<br/> {pwAdv} </div>
+                            <div id="password">Special:<br/> {pwSpecial}</div>
+                        </div>
 
-                    <div className="entropy">
-                        <p>Entropy:{entropy}</p>
-                    </div>
+                        <div className="entropy">
+                            <p>Entropy:{entropy}</p>
+                        </div>
 
-                    <button id="generate-button" onClick={this.show}>Go back</button>
+                        <button id="generate-button" onClick={this.show}>Go back</button>
+                    </div>
                 </div>
+
             )
         }
     }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h1>PASS PHRASE GENERATOR</h1>
+        </div>
+    )
+}
+
+export default Header
