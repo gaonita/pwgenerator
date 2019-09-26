902fc32a98f43b4b251ef8e6469a3f0a1f6371ed
gaonita <gaon.yg@gmail.com>
2019-09-26 15:27:27 +0200

    added how to use header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, {Component} from 'react';
 import './App.css';
 import Header from "./Header";
-import Main from "./Main";
-import Animation from "./Animation";
 import Slick from "./Slick";
-import FirstVersion from "./firstVersion";
-import {Switch, Route} from 'react-router-dom';
 
+// import Main from "./Main";
+// import Animation from "./Animation";
+// import FirstVersion from "./firstVersion";
+// import {Switch, Route} from 'react-router-dom';
+//
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Header/>
-            <div>
-                <Switch>
-                    <Route exact path='/' component={Main}></Route>
-                    <Route exact path='/version3' component={Slick}></Route>
-                    <Route exact path='/version2' component={Animation}></Route>
-                    <Route exact path='/version1' component={FirstVersion}></Route>
-                </Switch>
-            </div>
+                <Slick/>
+            {/*<div>*/}
+                {/*<Switch>*/}
+                    {/*<Route exact path='/' component={Main}></Route>*/}
+                    {/*<Route exact path='/version3' component={Slick}></Route>*/}
+                    {/*<Route exact path='/version2' component={Animation}></Route>*/}
+                    {/*<Route exact path='/version1' component={FirstVersion}></Route>*/}
+                {/*</Switch>*/}
+            {/*</div>*/}
             </div>
         )
     }
--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,13 @@ import React from 'react';
 const Header = () => {
     return(
         <div className="header">
-            <h1>PASS PHRASE GENERATOR</h1>
+            <h1>Generate random phrase and use it as password</h1>
+            <ol>
+            <li>1. Press the spin button</li>
+            <li>2. Press the stop button</li>
+            <li>3. Use the randomly generated phrase for your password</li>
+            </ol>
         </div>
-
-
     )
 };
 
